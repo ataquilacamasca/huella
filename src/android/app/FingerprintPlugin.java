dee8d6b0744d22af03c46ee92881e1dc07fed6f4
ataquilacamasca <a.taquilacamasca@gmail.com>
2025-07-30 15:38:10 -0500

    Update FingerprintPlugin.java

--- src/android/app/FingerprintPlugin.java
+++ src/android/app/FingerprintPlugin.java
@@ -1,5 +1,50 @@
 package com.fingerprint;
 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.util.Log;
+import android.content.Context;
+
+// Suponiendo que esta es la clase del SDK
+import com.touchlessid.FingerprintSDK;
+
+public class FingerprintPlugin extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if ("authenticate".equals(action)) {
+            this.authenticate(callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void authenticate(CallbackContext callbackContext) {
+        Context context = this.cordova.getActivity().getApplicationContext();
+
+        try {
+            // Aquí llamamos al SDK de huella
+            boolean success = FingerprintSDK.authenticate(context);
+
+            if (success) {
+                callbackContext.success("Autenticación exitosa");
+            } else {
+                callbackContext.error("Fallo en la autenticación");
+            }
+
+        } catch (Exception e) {
+            Log.e("FingerprintPlugin", "Error en autenticación", e);
+            callbackContext.error("Excepción: " + e.getMessage());
+        }
+    }
+}
+
+/*
+package com.fingerprint;
+
 import com.touchlessid.TouchLessIDSDK;
 
 import android.content.Context;
@@ -10,6 +55,7 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 
 
 public class FingerprintPlugin extends CordovaPlugin {
@@ -43,3 +89,4 @@ public class FingerprintPlugin extends CordovaPlugin {
         }
     }
 }
+*/
