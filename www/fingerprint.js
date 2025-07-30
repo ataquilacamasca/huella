f4ee1323026121adc49cad136b2d888c2920915c
mvhm1990 <mhuerta@canvia.com>
2025-07-29 20:09:50 -0500

    subiendo por favor

--- www/fingerprint.js
+++ www/fingerprint.js
@@ -1,7 +1,28 @@
 var exec = require('cordova/exec');
+
 var FingerprintPlugin = {
-  autenticar: function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "FingerprintPlugin", "autenticar", []);
+  autenticar: function (successCallback, errorCallback) {
+    exec(
+      function (result) {
+        // Convierte el resultado JSON de string a objeto si es necesario
+        if (typeof result === "string") {
+          try {
+            result = JSON.parse(result);
+          } catch (e) {
+            return errorCallback("Error al parsear respuesta: " + e.message);
+          }
+        }
+
+        successCallback(result);
+      },
+      function (err) {
+        errorCallback("Autenticación fallida: " + err);
+      },
+      "FingerprintPlugin",
+      "autenticar",
+      []
+    );
   }
 };
+
 module.exports = FingerprintPlugin;
